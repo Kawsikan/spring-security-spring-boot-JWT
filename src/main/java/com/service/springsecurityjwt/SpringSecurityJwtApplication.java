55eb3b8571696b3cd7c157c1b98041eb78216ab8
kawsikan <kawsinote@gmail.com>
2021-07-29 23:00:47 +0530

    created test data with CommandLineRunner

--- src/main/java/com/service/springsecurityjwt/SpringSecurityJwtApplication.java
+++ src/main/java/com/service/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -1,13 +1,45 @@
 package com.service.springsecurityjwt;
 
+import com.service.springsecurityjwt.domain.Role;
+import com.service.springsecurityjwt.domain.User;
+import com.service.springsecurityjwt.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SpringSecurityJwtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityJwtApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            userService.saveRole(new Role(null, "ROLE_USER"));
+            userService.saveRole(new Role(null, "ROLE_MANAGER"));
+            userService.saveRole(new Role(null, "ROLE_ADMIN"));
+            userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+            userService.saveUser(new User(null, "Kawsikan Kumaralingam", "Kawsikan", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null,"Michael John","Michael","1234",new ArrayList<>()));
+            userService.saveUser(new User(null,"Pamal Ranasinghe ","Pamal","1234",new ArrayList<>()));
+            userService.saveUser(new User(null,"Jeyakody Didula","Jeyakody","1234",new ArrayList<>()));
+
+            userService.addRoleToUser("Kawsikan","ROLE_SUPER_ADMIN");
+            userService.addRoleToUser("Kawsikan","ROLE_USER");
+            userService.addRoleToUser("Kawsikan","ROLE_MANAGER");
+            userService.addRoleToUser("Michael","ROLE_MANAGER");
+            userService.addRoleToUser("Pamal","ROLE_ADMIN");
+            userService.addRoleToUser("Jeyakody","ROLE_USER");
+
+
+
+        };
+    }
 
 }
