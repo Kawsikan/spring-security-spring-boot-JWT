01a9cd8fc96c856cf5317d9518b70a814db27401
kawsikan <kawsinote@gmail.com>
2021-07-30 12:39:08 +0530

    initial jwt connection working

--- src/main/java/com/service/springsecurityjwt/SpringSecurityJwtApplication.java
+++ src/main/java/com/service/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -15,6 +17,11 @@ public class SpringSecurityJwtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityJwtApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
--- src/main/java/com/service/springsecurityjwt/service/UserServiceImpl.java
+++ src/main/java/com/service/springsecurityjwt/service/UserServiceImpl.java
@@ -6,24 +6,49 @@ import com.service.springsecurityjwt.repository.RoleRepository;
 import com.service.springsecurityjwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            log.error("user not found in the database");
+            throw new UsernameNotFoundException("user not found in the database");
+        } else {
+            log.error("user found in the database: {}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role ->
+        {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
 
     @Override
     public User saveUser(User user) {
         log.info("Saving new user {} to the database", user.getName());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
@@ -52,4 +77,5 @@ public class UserServiceImpl implements UserService {
         log.info("Fetching all users");
         return userRepository.findAll();
     }
+
 }
