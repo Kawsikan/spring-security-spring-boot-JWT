eec3687d95aa742b2341421878567ca9664405d7
kawsikan <kawsinote@gmail.com>
2021-07-31 10:09:11 +0530

    authenticated routes

--- src/main/java/com/service/springsecurityjwt/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/service/springsecurityjwt/filter/CustomAuthenticationFilter.java
@@ -62,6 +62,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
 //        response.setHeader("access_token", access_token);
 //        response.setHeader("refresh_token", refresh_token);
+
         Map<String, String> tokens = new HashMap<>();
         tokens.put("access_token", access_token);
         tokens.put("refresh_token", refresh_token);
