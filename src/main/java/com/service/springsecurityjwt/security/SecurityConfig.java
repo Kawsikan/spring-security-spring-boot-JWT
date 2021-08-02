0d79ede7f930751c77f305c99484f86156ec7125
kawsikan <kawsinote@gmail.com>
2021-08-02 22:09:05 +0530

    added custom authorization

--- src/main/java/com/service/springsecurityjwt/security/SecurityConfig.java
+++ src/main/java/com/service/springsecurityjwt/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.service.springsecurityjwt.security;
 
 import com.service.springsecurityjwt.filter.CustomAuthenticationFilter;
+import com.service.springsecurityjwt.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
@@ -40,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
